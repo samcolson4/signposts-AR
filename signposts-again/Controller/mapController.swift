027507635ab6a8acae24869b8c6ff4c054fef8d9
samcolson4 <samcolson4@gmail.com>
2020-11-11 14:51:46 +0000

    Add map storyboard and mapController

---
+++ signposts-again/Controller/mapController.swift
@@ -0,0 +1,22 @@
+//
+//  mapController.swift
+//  signposts-again
+//
+//  Created by Sam Colson on 11/11/2020.
+//
+
+import UIKit
+import Firebase
+import CoreLocation
+import mapKit
+
+class ViewController: mapController {
+     
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
+
