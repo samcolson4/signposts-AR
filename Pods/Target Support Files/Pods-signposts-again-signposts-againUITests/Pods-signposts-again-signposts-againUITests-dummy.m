41c38a089af1b0145c04d5038dfe3ade3a485cd8
samcolson4 <samcolson4@gmail.com>
2020-11-11 10:37:32 +0000

    Delete pods

--- Pods/Target Support Files/Pods-signposts-again-signposts-againUITests/Pods-signposts-again-signposts-againUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_signposts_again_signposts_againUITests : NSObject
-@end
-@implementation PodsDummy_Pods_signposts_again_signposts_againUITests
-@end
--- Pods/Target Support Files/Pods-signposts-again/Pods-signposts-again-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_signposts_again : NSObject
-@end
-@implementation PodsDummy_Pods_signposts_again
-@end
