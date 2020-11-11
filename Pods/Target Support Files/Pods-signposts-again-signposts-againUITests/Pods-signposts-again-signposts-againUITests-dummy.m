d5f876cb7b8abd517edf460553356f1e04ecf519
Louis Kirkham <chew@talk21.com>
2020-11-11 09:37:40 +0000

    added podfile

---
+++ Pods/Target Support Files/Pods-signposts-again-signposts-againUITests/Pods-signposts-again-signposts-againUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_signposts_again_signposts_againUITests : NSObject
+@end
+@implementation PodsDummy_Pods_signposts_again_signposts_againUITests
+@end
---
+++ Pods/Target Support Files/Pods-signposts-again/Pods-signposts-again-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_signposts_again : NSObject
+@end
+@implementation PodsDummy_Pods_signposts_again
+@end
