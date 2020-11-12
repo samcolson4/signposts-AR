883fa8a67b1e68d7c0c3057c22b54712acd94412
CRTW26 <worthington.connor@gmail.com>
2020-11-12 14:22:52 +0000

    Add box to text

--- signposts-again/Controller/ARViewController.swift
+++ signposts-again/Controller/ARViewController.swift
@@ -20,16 +20,33 @@ class ARViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-
-        let message = MeshResource.generateText(text, extrusionDepth: 0.1, font: .systemFont(ofSize: 0.2))
+        // Box for sign
+        let box = MeshResource.generateBox(width: 0.3, height: 0.3, depth: 0.1)
+        let boxMaterial = SimpleMaterial(color: .red, isMetallic: false)
+        let entityBox = ModelEntity(mesh: box, materials: [boxMaterial])
         
+        let boxAnchor = AnchorEntity(plane: .horizontal)
+        boxAnchor.addChild(entityBox)
+//        sceneView.scene.addAnchor(boxAnchor)
+        
+        // Text
+        let message = MeshResource.generateText(text, extrusionDepth: 0.2, font: .systemFont(ofSize: 0.2))
         let material = SimpleMaterial(color: .blue, isMetallic: false)
-        let entity = ModelEntity(mesh: message, materials: [material])
-
-        let anchor = AnchorEntity(plane: .horizontal)
-        anchor.addChild(entity)
-        sceneView.scene.addAnchor(anchor)
-
+        let entityText = ModelEntity(mesh: message, materials: [material])
+        
+       
+//        let anchor = AnchorEntity(plane: .horizontal)
+//        anchor.addChild(entityText)
+//        sceneView.scene.addAnchor(anchor)
+//
+        entityBox.addChild(entityText)
+//    boxAnchor.addChild(entityBox)
+        entityText.setPosition(SIMD3<Float>(-0.1, -0.1, 0), relativeTo: entityBox)
+        sceneView.scene.addAnchor(boxAnchor)
+        
+        entityBox.generateCollisionShapes(recursive: true)
+        sceneView.installGestures([.translation, .rotation, .scale], for: entityBox)
+        
 //        let box = MeshResource.generateBox(size: 0.3)
 //        let boxAnchor = try! Experience.loadBox()
 //        let unknown = boxAnchor.emptySign?.children[0]
