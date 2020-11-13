6843e3215d065c026d3afbd189643746f2ddf5fc
samcolson4 <samcolson4@gmail.com>
2020-11-13 10:33:02 +0000

    Checkout to mapview

---
+++ signposts-again/Controller/arViewController.swift
@@ -0,0 +1,21 @@
+//
+//  arViewController.swift
+//  
+//
+//  Created by Sam Colson on 13/11/2020.
+//
+
+import UIKit
+import ARKit
+import RealityKit
+import CoreLocation
+
+class ARViewController: UIViewController {
+    
+    @IBOutlet weak var arViewNew: ARView!
+    
+    override func viewDidLoad() {
+        
+    }
+        
+}
