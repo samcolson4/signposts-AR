be9be7754506905958f790f89060607321bbe718
Louis Kirkham <chew@talk21.com>
2020-11-16 10:49:38 +0000

    added login controller errors

--- signposts-again/Controller/RegisterViewController.swift
+++ signposts-again/Controller/RegisterViewController.swift
@@ -10,26 +10,52 @@ import Firebase
 
 class RegisterViewController: UIViewController {
 
-    @IBOutlet var emailForm: UITextField!
-    @IBOutlet var passwordForm: UITextField!
-    @IBOutlet var registerButton: UIButton!
+    @IBOutlet weak var usernameForm: UITextField!
+    @IBOutlet weak var emailForm: UITextField!
+    @IBOutlet weak var passwordForm: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    @IBOutlet weak var registerErrorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.hidesBackButton = false
+    
+        passwordForm.addTarget(self,
+                               action: #selector(checkPasswordAndDisplayError(passwordForm:)),
+                               for: .editingChanged)
     }
     
     @IBAction func registerBtnPressed(_ sender: UIButton) { registerNewUser() }
     
     func registerNewUser() {
-        if let email = emailForm.text, let password = passwordForm.text {
+        if let email = emailForm.text, let password = passwordForm.text, let username = usernameForm.text  {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error {
-                    print(e.localizedDescription)
+                    self.registerErrorLabel.text = e.localizedDescription
                 } else {
+                    self.editUsername(username: username)
                     self.performSegue(withIdentifier: "registered", sender: self)
                 }
             }
         }
+    }
+    
+    @objc func checkPasswordAndDisplayError (passwordForm: UITextField) {
+        if (passwordForm.text?.count ?? 0 <= 5) {
+            passwordErrorLabel.text = "Password must be 6 or more characters"
+        } else {
+            passwordErrorLabel.text = ""
+        }
+    }
+    
+    //this method might be duplicated in profile controller so maybe extraction needed?
+    func editUsername(username: String) {
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        changeRequest?.displayName = username
+        changeRequest?.commitChanges(completion: { (error) in
+            if let e = error {
+                print(e.localizedDescription)
+            }
+        })
     }
 }
