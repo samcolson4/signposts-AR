d3c23bddaa5519149053a6d6e66004fd61344903
Sam Colson <samcolson4@gmail.com>
2020-11-17 10:13:24 +0000

    Fancyannotation (#11)
    * General refactor. Fail to fix tab bar issues.
    
    * Annotation colour / symbol change
    
    * Add user location to map view
    
    * Refactor
    
    * Add subtitles.
    
    * Refactor
    
    * fixed navigation and tab bars on map
    
    * Rename to explore tab
    
    * Update map constraints.
    
    Co-authored-by: Louis Kirkham <chew@talk21.com>

--- signposts-again/Controller/MapController.swift
+++ signposts-again/Controller/MapController.swift
@@ -10,14 +10,23 @@ import Firebase
 import CoreLocation
 import MapKit
 
-class MapController: UIViewController {
+class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     let library = SignLibrary()
     var documents = [QueryDocumentSnapshot]()
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var signmap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        signmap.delegate = self
+        signmap.mapType = MKMapType.standard
+        signmap.showsUserLocation = true
+        
         displaySigns()
     }
     
@@ -37,15 +46,42 @@ class MapController: UIViewController {
                 }
             
             for sign in signArray {
-                let annotation = MKPointAnnotation()
+                let annotation = signAnnotation()
         
                 let latitude = sign.location.latitude
                 let longtitude = sign.location.longitude
     
                 annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
                 annotation.title = sign.message
-                self.signmap.addAnnotation(annotation)
+                annotation.subtitle = sign.username
+//                annotation.pinCustomImageName = "1024Transparent"
+                            
+                let pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+                pinAnnotationView.canShowCallout = false
+
+                self.signmap.addAnnotation(pinAnnotationView.annotation!)
+                
+//                self.signmap.addAnnotation(annotation)
             }
         })
     }
 }
+
+
+//extension MapController {
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//
+//          let Identifier = "Pin"
+//          let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Identifier) ?? MKAnnotationView(annotation: annotation, reuseIdentifier: Identifier)
+//
+//          annotationView.canShowCallout = true
+//          if annotation is MKUserLocation {
+//             return nil
+//          } else if annotation is signAnnotation {
+//            annotationView.image = UIImage(contentsOfFile: "signpostsyellow")
+//             return annotationView
+//          } else {
+//             return nil
+//          }
+//       }
+//}
