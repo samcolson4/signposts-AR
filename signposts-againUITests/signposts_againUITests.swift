064fc0e508c15764d720a869e6723a48acb999e8
Victoria Jusko <vikjusko@gmail.com>
2020-11-18 19:46:48 +0000

    editing feature test

--- signposts-againUITests/signposts_againUITests.swift
+++ signposts-againUITests/signposts_againUITests.swift
@@ -194,6 +194,11 @@ class signposts_againUITests: XCTestCase {
     }
     
     
+    func testSignUpEmailEmpty() {
+                
+                
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
