c55da65e372c083376d05bcdfd42680152a05597
Victoria Jusko <vikjusko@gmail.com>
2020-11-18 16:30:57 +0000

    feature tests for login page

--- signposts-againUITests/signposts_againUITests.swift
+++ signposts-againUITests/signposts_againUITests.swift
@@ -22,20 +22,62 @@ class signposts_againUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testloginExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        XCTAssert(app.textFields["email"].exists)
+        XCTAssert(app.secureTextFields["password"].exists)
+        XCTAssert(app/*@START_MENU_TOKEN@*/.staticTexts["Log In"]/*[[".buttons[\"Log In\"].staticTexts[\"Log In\"]",".staticTexts[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists)
+        XCTAssertFalse(app.staticTexts["Profile"].exists)
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testLoginAttempt() throws {
+        let app = XCUIApplication()
+        app.launch()
+        XCUIApplication()/*@START_MENU_TOKEN@*/.staticTexts["Log In"]/*[[".buttons[\"Log In\"].staticTexts[\"Log In\"]",".staticTexts[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.staticTexts["The password is invalid or the user does not have a password."].exists)
+//        XCTAssert(app.staticTexts["There is no user record corresponding to this identifier"].exists)
+        
+    }
+    
+    func testcorrectLogin() {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields["email"].tap()
+        let textFields = app.textFields["email"]
+        textFields.typeText("vikjusko@gmail.com")
+        let secureTextFields = app.secureTextFields["password"]
+        app.secureTextFields["password"].tap()
+        secureTextFields.typeText("5433001Vika")
+        app/*@START_MENU_TOKEN@*/.staticTexts["Log In"]/*[[".buttons[\"Log In\"].staticTexts[\"Log In\"]",".staticTexts[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertFalse(app.staticTexts["There is no user record corresponding to this identifier"].exists)
+    }
+    
+    func testLoginLogout() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        let emailTextField = app.textFields["email"]
+        emailTextField.tap()
+        emailTextField.typeText("vikjusko@gmail.com")
+        let passwordSecureTextField = app.secureTextFields["password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("5433001Vika")
+        app/*@START_MENU_TOKEN@*/.staticTexts["Log In"]/*[[".buttons[\"Log In\"].staticTexts[\"Log In\"]",".staticTexts[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.tabBars["Tab Bar"].buttons["person"].tap()
+        app.buttons["Signout"].tap()
+        XCTAssertFalse(app.staticTexts["Profile"].exists)
+    }
+    
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
+                
+               
             }
         }
     }
