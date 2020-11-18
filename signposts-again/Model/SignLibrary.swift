d60bba89e069d52238fb0f803d4138c36b7c1137
Victoria Jusko <vikjusko@gmail.com>
2020-11-18 12:20:51 +0000

    working on data download

--- signposts-again/Model/SignLibrary.swift
+++ signposts-again/Model/SignLibrary.swift
@@ -16,15 +16,16 @@ class SignLibrary {
     let storage = Storage.storage()
     let locationManager = CLLocationManager()
     let user = Auth.auth().currentUser
+
     
     func addNewSign(message: String, worldMapData: Data) {
         let date = Date()
         var ref: DocumentReference? = nil
         let currentLoc: CLLocation
         currentLoc = locationManager.location!
-        let username = user?.displayName
         let geolocation = GeoPoint(latitude: currentLoc.coordinate.latitude,
                                    longitude: currentLoc.coordinate.longitude)
+        let username = user?.displayName
         ref = db.collection("signs").addDocument(data: ["message": message,
                                                         "geolocation": geolocation, "created": date, "username": username ?? ""]) {
             err in if let err = err {
@@ -62,6 +63,7 @@ class SignLibrary {
     }
     
     func uploadWorldMap(worldMapData: Data, filename: String) {
+        
         let storageRef = storage.reference()
         let worldMapRef = storageRef.child("worldmap") // location storage ref
         let mapRef = worldMapRef.child(filename)
@@ -81,4 +83,35 @@ class SignLibrary {
             }
         }
     }
-}
+    
+//    func downloadSign() -> String {
+//        var signArray = [Sign]()
+//        let currentLoc: CLLocation
+//        currentLoc = locationManager.location!
+//        let geolocation = GeoPoint(latitude: currentLoc.coordinate.latitude,
+//                                   longitude: currentLoc.coordinate.longitude)
+//
+//        self.returnDocs(completion: { (status, signs) in print(status, signs)
+//
+//            for object in signs {
+//                let message = object.data()["message"]
+//                let date = object.data()["created"]
+//                let location = object.data()["geolocation"]
+//                let username = object.data()["username"]
+//
+//                let newSign = Sign(message: message as! String, date: date as! Timestamp, location: location as! GeoPoint, username: username as? String)
+//
+//                if newSign.location == geolocation{
+//                        signArray.append(newSign)
+//                    }
+//                }
+//             for sign in signArray {
+//                return sign.message
+//            }
+//
+//        })
+//    }
+        
+        
+        }
+
